680b61ee217b1747ae130e82410cbe037166762b
Kirill <kirill.kanabay@gmail.com>
2021-05-22 21:53:53 +0300

    Исправление опечатки

--- Cryptex/ViewModels/RsaCryptography/EncryptMessageViewModel.cs
+++ Cryptex/ViewModels/RsaCryptography/EncryptMessageViewModel.cs
@@ -61,7 +61,7 @@ namespace Cryptex.ViewModels.RsaCryptography
             set
             {
                 _plainTextCardIsEnabled = value;
-                OnPropertyChanged(nameof(PlaintTextCardIsEnabled));
+                OnPropertyChanged(nameof(PlainTextCardIsEnabled));
             }
         }
 
